dfc9a06494529f812de007d4e46b289e6b6ca8b9
petrashaker <petra.shaker@gmail.com>
2021-09-13 21:35:09 +0200

    Dořešena SearchBar komponenta - viditelný ul a li

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 import './SearchBar.css';
 
 //properties from yelp documentation https://www.yelp.com/developers/documentation/v3/business_search
 
-//codecacemy body 28 - 43   
+//Ravenous part 1 - codecacemy body 28 - 43   
 
 // const sortByOptions = {
 //     'Best Match': best_match,
@@ -21,20 +21,21 @@ import './SearchBar.css';
 
 //zakomentováno, protože React hlásí chyby v renderu, nicméně stejně třeba dořešit
 export default function SearchBar() {
-    const [sortByOptions] = useState({
+    const sortByOptions = {
         'Best Match':  'Chosen restaurean',
         'Highest Rated': 5,
         'Most Reviewed': 23
         // 'Best Match': best_match,
         // 'Highest Rated': rating,
         // 'Most Reviewed': review_count
-    });
-    const renderSortByOptions = (sortByOptions) => { 
-        sortByOptions.keys
-        .map(sortByOption => 
-            <li key={sortByOption}>{sortByOptions[sortByOption]}</li>
+    };
+
+    const renderSortByOptions = 
+        Object.keys(sortByOptions)
+        .map(sortByOptions => 
+            <li>{sortByOptions}</li>
         )
-    }
+    
 
     return ( 
         <div className="SearchBar">
@@ -49,5 +50,5 @@ export default function SearchBar() {
                 <a>Let's Go</a>
             </div>
     </div>
-)       
+    )       
 }
